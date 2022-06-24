31b0286cdd624f7cbbb35a17cabb8267c865d94f
Eucleth <eucleth.contreras@gmail.com>
2022-06-24 15:38:25 +0200

    Fixes

--- src/main/java/com/siw/secondHand/repository/UserRepository.java
+++ src/main/java/com/siw/secondHand/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.siw.secondHand.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.siw.secondHand.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	public List<User> findByOrderByCognomeAscNome();
 }
